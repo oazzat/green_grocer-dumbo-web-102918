e380189bc9982cd92e2f83423a567c17ff3cefd4
Omar A <oazzat19@gmail.com>
2018-10-28 17:39:58 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,78 @@
+require "pry"
+
 def consolidate_cart(cart)
-  # code here
+  array = []
+  hash = {}
+  cart.each do |item|
+    item.each do |key,val|
+      if !hash.include?(key)
+        hash[key] = val
+        hash[key][:count] = 1
+      else
+        hash[key][:count] +=1
+      end
+    end
+  end
+  hash
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  hash = cart
+  counter = 0
+  
+  while counter < coupons.length 
+  item = coupons[counter][:item]
+  
+    if cart.include?(coupons[counter][:item]) && !hash.key?("#{item} W/COUPON")
+        hash["#{item} W/COUPON"] = {price: coupons[counter][:cost],clearance: cart[item][:clearance], count: 0}
+      hash["#{item} W/COUPON"][:count] +=1
+      hash[item][:count] -= coupons[counter][:num]
+      
+    elsif hash.key?("#{item} W/COUPON")
+    hash["#{item} W/COUPON"][:count] +=1
+    hash[item][:count] -= coupons[counter][:num]
+    end
+    
+    counter +=1 
+  end
+  hash
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.collect do |key,val|
+    if val[:clearance]
+      val[:price] = (val[:price] * 0.8).round(2)
+    end
+  end 
+  cart
 end
 
 def checkout(cart, coupons)
-  # code here
+  total = 0.0
+  
+  if cart.length == 1
+    cart = consolidate_cart(cart)
+    cart.each do |key,val|
+      total = val[:price]
+    end
+    apply_coupons(cart)
+    apply_clearance(cart)
+    
+  
+  else
+  cart.each do |item, value|
+    coupons.each do |coupon|
+      if coupon[:item] == item && coupon[:num]<= cart[:count]
+        apply_coupons(cart,coupons)
+      end
+    end
+  end
+  end
+  
+  apply_clearance(cart)
+  cart.each do |key,val|
+      total += val[:price]
+  end
+  
+  return total
 end
